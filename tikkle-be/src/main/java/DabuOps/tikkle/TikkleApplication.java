1996732b29c86827ead52551779ca0ebb0997693
SSH <school023@gmail.com>
2023-03-06 16:18:55 +0900

    [생성] 프로젝트 생성

---
+++ tikkle-be/src/main/java/DabuOps/tikkle/TikkleApplication.java
@@ -0,0 +1,13 @@
+package DabuOps.tikkle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TikkleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TikkleApplication.class, args);
+	}
+
+}
---
+++ tikkle-be/src/test/java/DabuOps/tikkle/TikkleApplicationTests.java
@@ -0,0 +1,13 @@
+package DabuOps.tikkle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TikkleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
