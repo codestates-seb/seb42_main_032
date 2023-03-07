3c695fa9c325a1881f915dca8f2bfff99c385177
SSH <school023@gmail.com>
2023-03-07 11:38:37 +0900

    [수정] Member.java

--- tikkle-be/src/main/java/DabuOps/tikkle/member/entity/Member.java
+++ tikkle-be/src/main/java/DabuOps/tikkle/member/entity/Member.java
@@ -1,8 +1,13 @@
 package DabuOps.tikkle.member.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Member {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long memberId;
 }
