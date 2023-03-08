b9337a53c089782758548dfb2f84248e68c4d1cf
ImGyuriKim <gyurikim0105@gmail.com>
2023-03-08 15:39:00 +0900

    Set directory structure

---
+++ tikkle-fe/src/pages/BudgetSetting.tsx
@@ -0,0 +1,12 @@
+//TODO BUDGET_001 예산 설정 페이지 구현
+import React from 'react'
+
+function BudgetSetting() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default BudgetSetting
---
+++ tikkle-fe/src/pages/UserSetting.tsx
@@ -0,0 +1,12 @@
+//TODO SIGNUP_002 유저 정보 입력 페이지 구현 (사용자 이름, 예산 시작일, 급여일, 고정 지출 등)
+import React from 'react'
+
+function UserSetting() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default UserSetting
