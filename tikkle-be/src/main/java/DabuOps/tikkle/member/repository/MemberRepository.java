16b6ae1b8c71895eac00eb6e75b6d7fe2fab65e1
SSH <school023@gmail.com>
2023-03-09 16:27:57 +0900

    [ADD] MemberService.java, MemberRepository.java

--- tikkle-be/src/main/java/DabuOps/tikkle/member/repository/MemberRepository.java
+++ tikkle-be/src/main/java/DabuOps/tikkle/member/repository/MemberRepository.java
@@ -1,8 +1,10 @@
 package DabuOps.tikkle.member.repository;
 
+import DabuOps.tikkle.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MemberRepository {
+public interface MemberRepository extends JpaRepository<Member, Long> {
 
 }
