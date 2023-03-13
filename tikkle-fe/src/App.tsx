807ff9c943508ed3bf4eb1a54d215e8e7bdbb651
ImGyuriKim <gyurikim0105@gmail.com>
2023-03-13 13:57:34 +0900

    Add transactino box component

--- tikkle-fe/src/App.tsx
+++ tikkle-fe/src/App.tsx
@@ -1,12 +1,38 @@
-import './App.css'
-import Calendar from './components/layout/Calendar'
+import './App.css';
+import Calendar from './components/layout/Calendar';
+import Transaction from './components/layout/Transaction';
 
+// Transaction 컴포넌트용 거래내역 dummydata
+const transactions: Transaction[] = [
+  {
+    date: new Date('2023-03-01'),
+    bankInfo: '국민은행',
+    payee: '버거킹',
+    category: '식비',
+    amount: -9200,
+  },
+  {
+    date: new Date('2023-03-03'),
+    bankInfo: '우리은행',
+    payee: '스타벅스',
+    category: '음료',
+    amount: -5200,
+  },
+  {
+    date: new Date('2023-03-06'),
+    bankInfo: '토스뱅크',
+    payee: '볼링장',
+    category: '오락',
+    amount: -30000,
+  },
+];
 function App() {
   return (
     <div className="App">
       <Calendar date={new Date()} />
+      <Transaction transactions={transactions} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- tikkle-fe/src/components/layout/Transaction.tsx
+++ tikkle-fe/src/components/layout/Transaction.tsx
@@ -1,12 +1,116 @@
 //TODO 거래내역 박스
-import React from 'react'
+import { FC } from 'react';
+import styled from 'styled-components';
+import { MdFastfood } from 'react-icons/md';
+import { BiCoffeeTogo } from 'react-icons/bi';
+import { IoLogoGameControllerA } from 'react-icons/io';
 
-function Transaction() {
-  return (
-    <div>
-      
-    </div>
-  )
+interface Transaction {
+  date: Date;
+  bankInfo: string;
+  payee: string;
+  category: string;
+  amount: number;
 }
 
-export default Transaction
+interface TransactionList {
+  transactions: Transaction[];
+}
+
+// 카테고리별 아이콘 설정하기 <카테고리명: 아이콘이름>
+const categoryIcons: Record<string, any> = {
+  식비: MdFastfood,
+  음료: BiCoffeeTogo,
+  오락: IoLogoGameControllerA,
+};
+
+// 카테고리별 아이콘 색상 설정하기
+const CategoryIconWrapper = styled.div<{ category: string }>`
+  max-width: fit-content;
+  margin-left: 20px;
+  padding: 10px;
+  border-radius: 100%;
+  color: white;
+  font-size: 20px;
+  background-color: ${(props) => {
+    switch (props.category) {
+      case '식비':
+        return '#FFC107'; // yellow
+      case '음료':
+        return '#4CAF50'; // green
+      case '오락':
+        return '#9C27B0'; // purple
+      default:
+        return 'inherit';
+    }
+  }};
+`;
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const TransactionContainer = styled.div`
+  padding: 10px;
+  .transaction-date-box {
+    text-align: left;
+    margin-top: 10px;
+    margin-bottom: 10px;
+    color: grey;
+  }
+`;
+
+const ContentContainer = styled.div`
+  display: flex;
+  .transaction-content-box {
+    margin-left: 20px;
+    text-align: left;
+  }
+  .transaciton-bank-box {
+    color: grey;
+  }
+`;
+
+// 날짜+요일별 거래내역 박스
+const Transaction: FC<TransactionList> = ({ transactions }) => {
+  const daysOfWeek = ['일', '월', '화', '수', '목', '금', '토'];
+
+  return (
+    <Container className="transaction-history-box">
+      {transactions.map((transaction, index) => {
+        const IconComponent = categoryIcons[transaction.category];
+        return (
+          <TransactionContainer>
+            <div key={index}>
+              <div className="transaction-date-box">
+                {transaction.date.getDate()}일{' '}
+                {daysOfWeek[transaction.date.getDay()]}요일
+              </div>
+              <ContentContainer>
+                <CategoryIconWrapper category={transaction.category}>
+                  <IconComponent className="transaciton-icon" />
+                </CategoryIconWrapper>
+
+                <div className="transaction-content-box">
+                  <div className="transaction-amount-box">
+                    <strong>
+                      {transaction.amount
+                        .toString()
+                        .replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ',')}
+                      원
+                    </strong>
+                  </div>
+                  <div className="transaciton-bank-box">
+                    {transaction.bankInfo} &#8594; {transaction.payee}
+                  </div>
+                </div>
+              </ContentContainer>
+            </div>
+          </TransactionContainer>
+        );
+      })}
+    </Container>
+  );
+};
+
+export default Transaction;
