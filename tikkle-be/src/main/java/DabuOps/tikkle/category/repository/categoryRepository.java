e1fc6bfd0d0c3add20df3e245d9fcf04cd87f69b
SSH <school023@gmail.com>
2023-03-14 01:59:10 +0900

    [fix] entity mapping 연관관계수정 밎 빌드, run 가능하게 수정

--- tikkle-be/src/main/java/DabuOps/tikkle/category/repository/categoryRepository.java
+++ tikkle-be/src/main/java/DabuOps/tikkle/category/repository/categoryRepository.java
@@ -1,6 +1,9 @@
 package DabuOps.tikkle.category.repository;
 
+import DabuOps.tikkle.category.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface categoryRepository extends JpaRepository {
+@Repository
+public interface categoryRepository extends JpaRepository<Category, Long> {
 }
