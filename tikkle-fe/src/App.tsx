3f947cb03d4df6ba902e474fa54e835cdeca47d2
Jaehun Kim <dev4jaehun.kim@gmail.com>
2023-03-14 17:24:11 +0900

    Remove unused code from App

--- tikkle-fe/src/App.tsx
+++ tikkle-fe/src/App.tsx
@@ -3,35 +3,9 @@ import { Routes, Route } from 'react-router-dom';
 import { useState } from 'react';
 import Home from './pages/Home';
 import Login from './pages/Login';
-import Calendar from './components/layout/Calendar';
-import Transaction from './components/layout/Transaction';
 import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
 
-// Transaction 컴포넌트용 거래내역 dummydata
-const transactions: Transaction[] = [
-  {
-    date: new Date('2023-03-01'),
-    bankInfo: '국민은행',
-    payee: '버거킹',
-    category: '식비',
-    amount: -9200,
-  },
-  {
-    date: new Date('2023-03-03'),
-    bankInfo: '우리은행',
-    payee: '스타벅스',
-    category: '음료',
-    amount: -5200,
-  },
-  {
-    date: new Date('2023-03-06'),
-    bankInfo: '토스뱅크',
-    payee: '볼링장',
-    category: '오락',
-    amount: -30000,
-  },
-];
 function App() {
   const [selectedDate, setSelectedDate] = useState<Date>(new Date());
   return (
@@ -42,8 +16,6 @@ function App() {
         <Route path="/login" element={<Login />} />
       </Routes>
       <Footer />
-      {/* <Calendar date={new Date()} />
-      <Transaction transactions={transactions} /> */}
     </div>
   );
 }
