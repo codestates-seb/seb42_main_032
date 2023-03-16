3b01b49a368f69e8f3dd9c41086c75f53cd12ed2
Jaehun Kim <dev4jaehun.kim@gmail.com>
2023-03-16 15:40:08 +0900

    Implement Add/Delete CategoryBudget

--- tikkle-fe/src/components/budget_setting/CategoryBudget.tsx
+++ tikkle-fe/src/components/budget_setting/CategoryBudget.tsx
@@ -13,9 +13,10 @@ import { useState } from 'react';
 
 // ToDo 추후 post, patch 요청 시 ID 값 활용
 interface categoryBudget {
-  categoryId: number;
+  categoryId?: number;
+  handleDelete: () => void;
 }
-const CategoryBudget = () => {
+const CategoryBudget = (props: categoryBudget) => {
   // ToDo 네트워크에 연결된 데이터를 수정하도록 변경
   const [budget, setBudget] = useState(0);
   const [isEditable, setIsEditable] = useState(false);
@@ -32,51 +33,54 @@ const CategoryBudget = () => {
   };
 
   return (
-    <Box fontFamily="GmarketSansMedium" w="60%" display="flex">
-      {/* Icon Container */}
-      <Box
-        display="flex"
-        justifyContent="center"
-        alignItems="center"
-        w="15%"
-        fontSize="2em"
-      >
-        <MdShoppingCart />
-      </Box>
-      {/* Input per category Container */}
-      <Box display="flex" justifyContent="space-between" w="85%">
-        {/* Category Label Container */}
-        <Box display="flex" flexDir="column" alignItems="flex-start" w="40%">
-          {isEditable ? (
-            <InputGroup>
-              <Input type="text" value={label} onChange={handleLabel}></Input>
-              <InputRightElement>
-                <Button onClick={handleEditable}>저장</Button>
-              </InputRightElement>
-            </InputGroup>
-          ) : (
-            <Box display="flex">
-              <Text>{label}</Text>
-              <Box
-                display="flex"
-                alignItems="center"
-                ml={1}
-                onClick={handleEditable}
-              >
-                <MdEdit color="grey" width={2} />
-              </Box>
-            </Box>
-          )}
-          <Text fontSize="0.8rem" color="grey">
-            지난달 0원
-          </Text>
+    <Box fontFamily="GmarketSansMedium" w="60%" display="flex" flexDir="column">
+      <Box display="flex" w="100%">
+        {/* Icon Container */}
+        <Box
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          w="15%"
+          fontSize="2em"
+        >
+          <MdShoppingCart />
         </Box>
-        {/* Input Container */}
-        <InputGroup w="50%">
-          <Input type="number" value={budget} onChange={handleBudget}></Input>
-          <InputRightAddon fontSize="0.8rem" children="원" />
-        </InputGroup>
+        {/* Input per category Container */}
+        <Box display="flex" justifyContent="space-between" w="85%">
+          {/* Category Label Container */}
+          <Box display="flex" flexDir="column" alignItems="flex-start" w="40%">
+            {isEditable ? (
+              <InputGroup>
+                <Input type="text" value={label} onChange={handleLabel}></Input>
+                <InputRightElement>
+                  <Button onClick={handleEditable}>저장</Button>
+                </InputRightElement>
+              </InputGroup>
+            ) : (
+              <Box display="flex">
+                <Text>{label}</Text>
+                <Box
+                  display="flex"
+                  alignItems="center"
+                  ml={1}
+                  onClick={handleEditable}
+                >
+                  <MdEdit color="grey" width={2} />
+                </Box>
+              </Box>
+            )}
+            <Text fontSize="0.8rem" color="grey">
+              지난달 0원
+            </Text>
+          </Box>
+          {/* Input Container */}
+          <InputGroup w="50%">
+            <Input type="number" value={budget} onChange={handleBudget}></Input>
+            <InputRightAddon fontSize="0.8rem" children="원" />
+          </InputGroup>
+        </Box>
       </Box>
+      <Button onClick={props.handleDelete}>삭제</Button>
     </Box>
   );
 };
--- tikkle-fe/src/pages/BudgetSetting.tsx
+++ tikkle-fe/src/pages/BudgetSetting.tsx
@@ -1,13 +1,27 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Button } from '@chakra-ui/react';
+import { useState } from 'react';
 import CategoryBudget from '../components/budget_setting/CategoryBudget';
 import MonthlyBudget from '../components/budget_setting/MonthlyBudget';
 
 const BudgetSetting = () => {
+  const [categoryList, setCategoryList] = useState([{}]);
+
+  const addCategory = () => {
+    setCategoryList([...categoryList, {}]);
+  };
+  const deleteCategory = () => {
+    setCategoryList(categoryList.slice(0, categoryList.length - 1));
+  };
+
   return (
     <>
       <MonthlyBudget />
-      <Box h="5rem" />
-      <CategoryBudget />
+      <Box h="5rem">
+        <Button onClick={addCategory}>+</Button>
+      </Box>
+      {categoryList.map(() => (
+        <CategoryBudget handleDelete={deleteCategory} />
+      ))}
     </>
   );
 };
