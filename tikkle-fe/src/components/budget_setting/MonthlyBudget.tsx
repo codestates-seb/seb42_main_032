94b07580723b5d6f8abdb19a609a19e1d2953516
Jaehun Kim <dev4jaehun.kim@gmail.com>
2023-03-16 10:16:55 +0900

    Add handleTotalBudget

--- tikkle-fe/src/components/budget_setting/MonthlyBudget.tsx
+++ tikkle-fe/src/components/budget_setting/MonthlyBudget.tsx
@@ -1,5 +1,12 @@
-import React from 'react';
-import { Box, Text } from '@chakra-ui/react';
+import React, { useState } from 'react';
+import {
+  Box,
+  Input,
+  InputGroup,
+  Button,
+  InputRightElement,
+  Text,
+} from '@chakra-ui/react';
 import { useRecoilState } from 'recoil';
 import { MdEdit } from 'react-icons/md';
 
@@ -7,9 +14,19 @@ import { MdEdit } from 'react-icons/md';
 import { dateState, totalBudgetState } from '../../util/store';
 
 const MonthlyBudget = () => {
-  const [date, setDate] = useRecoilState<Date>(dateState);
+  const [date] = useRecoilState<Date>(dateState);
   const [totalBudget, setTotalBudget] =
     useRecoilState<number>(totalBudgetState);
+
+  const [isEditable, setIsEditable] = useState(false);
+
+  const handleEditable = () => {
+    setIsEditable(!isEditable);
+  };
+  // 어떤 타입으로 지정해야 할지 생각해보기
+  const handleTotalBudget = (event) => {
+    setTotalBudget(event.target.value);
+  };
 
   return (
     <Box
@@ -21,12 +38,32 @@ const MonthlyBudget = () => {
       {/* getMonth()는 1월이 0 값이므로, +1을 해줘야 함 */}
       <Text fontSize="0.8rem">{`${date.getMonth() + 1}월 예산`}</Text>
       <Box display="flex">
-        <Text as="b" fontSize="1.5rem">{`${new Intl.NumberFormat().format(
-          totalBudget
-        )}원`}</Text>
-        <Box display="flex" alignItems="center" ml={1}>
-          <MdEdit color="grey" width={2} />
-        </Box>
+        {isEditable ? (
+          <InputGroup>
+            <Input
+              type="number"
+              value={totalBudget}
+              onChange={handleTotalBudget}
+            ></Input>
+            <InputRightElement>
+              <Button onClick={handleEditable}>저장</Button>
+            </InputRightElement>
+          </InputGroup>
+        ) : (
+          <>
+            <Text as="b" fontSize="1.5rem">{`${new Intl.NumberFormat().format(
+              totalBudget
+            )}원`}</Text>
+            <Box
+              display="flex"
+              alignItems="center"
+              ml={1}
+              onClick={handleEditable}
+            >
+              <MdEdit color="grey" width={2} />
+            </Box>
+          </>
+        )}
       </Box>
       <Box fontSize="0.8rem" display="flex" gap="8rem">
         <Text>지난 달 지출</Text>
