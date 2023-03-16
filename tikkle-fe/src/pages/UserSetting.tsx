ec787bef6a88b82c5ae0c2c1b99b2ebfa15cb263
ImGyuriKim <gyurikim0105@gmail.com>
2023-03-16 10:14:59 +0900

    Add page layout

--- tikkle-fe/src/pages/UserSetting.tsx
+++ tikkle-fe/src/pages/UserSetting.tsx
@@ -16,12 +16,22 @@ import { TbPigMoney } from 'react-icons/tb';
 import { GiReceiveMoney, GiExpense } from 'react-icons/gi';
 
 const Container = styled.div`
+  background-color: lightgrey;
   display: flex;
   flex-direction: column;
-
-  h1 {
-    margin-bottom: 8em;
+  font-family: 'GmarketSansMedium';
+  font-size: 20px;
+  .title-container {
     text-align: left;
+    margin-top: 10vh;
+    margin-bottom: 10vh;
+    background-color: #faf5ff;
+  }
+  .usersetting-title {
+    margin-left: 5vh;
+    text-align: left;
+    font-weight: bold;
+    font-size: 2rem;
   }
 
   input {
@@ -32,6 +42,12 @@ const Container = styled.div`
 
 const SetContainer = styled.div`
   display: flex;
+  button {
+    margin-bottom: 60px;
+    position: relative;
+    top: -80px;
+    left: 20px;
+  }
 `;
 // TODO 체크 아이콘 조건부 색상 변경 (green)
 // TODO 금액 입력 시 콤마 찍혀서 input에 출력
@@ -65,46 +81,55 @@ function UserSetting() {
   };
   return (
     <Container>
-      <h1>회원 정보 설정</h1>
+      <div className="title-container">
+        <span className="usersetting-title">회원 정보 설정</span>
+      </div>
       <SetContainer>
-        <InputGroup className="input-username">
-          <InputLeftElement
-            pointerEvents="none"
-            children={<Icon as={BsFillPersonFill} color="gray.400" />}
-          />
-          <Input
-            type="text"
-            size="sm"
-            focusBorderColor="purple.400"
-            onKeyUp={(e) => {
-              handleUserInput(e);
-            }}
-          ></Input>
-          {/* <InputRightElement children={<CheckIcon color="gray.300" />} /> */}
-        </InputGroup>
-        <Button
-          colorScheme="purple"
-          size="md"
-          variant="outline"
-          leftIcon={<AddIcon />}
-          loadingText="연결 중"
-        >
-          계좌 연결하기
-        </Button>
+        <div>
+          <p>사용자 이름</p>
+          <InputGroup className="input-username" ml="10vw">
+            <InputLeftElement
+              pointerEvents="none"
+              children={<Icon as={BsFillPersonFill} color="gray.400" />}
+            />
+            <Input
+              type="text"
+              size="sm"
+              focusBorderColor="purple.400"
+              onKeyUp={(e) => {
+                handleUserInput(e);
+              }}
+            ></Input>
+            {/* <InputRightElement children={<CheckIcon color="gray.300" />} /> */}
+          </InputGroup>
+          <Button
+            colorScheme="purple"
+            size="md"
+            variant="outline"
+            leftIcon={<AddIcon />}
+            loadingText="연결 중"
+          >
+            계좌 연결하기
+          </Button>
+        </div>
       </SetContainer>
       <SetContainer>
-        <InputGroup className="input-initialbudget">
+        <div>
+          <p>예산 시작일</p>
+          <InputGroup className="input-initialbudget" ml="10vw">
+            <InputLeftElement
+              pointerEvents="none"
+              children={<Icon as={TbPigMoney} color="gray.400" />}
+            />
+            <Input type="date" size="sm" focusBorderColor="purple.400"></Input>
+          </InputGroup>
+        </div>
+        <InputGroup className="input-initialbudget" size="sm" ml="20vw">
           <InputLeftElement
             pointerEvents="none"
             children={<Icon as={TbPigMoney} color="gray.400" />}
           />
-          <Input type="date" size="sm" focusBorderColor="purple.400"></Input>
-        </InputGroup>
-        <InputGroup className="input-initialbudget" size="sm">
-          <InputLeftElement
-            pointerEvents="none"
-            children={<Icon as={TbPigMoney} color="gray.400" />}
-          />
+
           <Input
             type="number"
             focusBorderColor="purple.400"
@@ -116,14 +141,17 @@ function UserSetting() {
       </SetContainer>
 
       <SetContainer>
-        <InputGroup className="input-salary">
-          <InputLeftElement
-            pointerEvents="none"
-            children={<Icon as={GiReceiveMoney} color="gray.400" />}
-          />
-          <Input type="date" size="sm" focusBorderColor="purple.400"></Input>
-        </InputGroup>
-        <InputGroup className="input-salary" size="sm">
+        <div>
+          <p>급여일</p>
+          <InputGroup className="input-salary" ml="10vw">
+            <InputLeftElement
+              pointerEvents="none"
+              children={<Icon as={GiReceiveMoney} color="gray.400" />}
+            />
+            <Input type="date" size="sm" focusBorderColor="purple.400"></Input>
+          </InputGroup>
+        </div>
+        <InputGroup className="input-salary" size="sm" ml="20vw">
           <InputLeftElement
             pointerEvents="none"
             children={<Icon as={GiReceiveMoney} color="gray.400" />}
@@ -137,14 +165,17 @@ function UserSetting() {
         </InputGroup>
       </SetContainer>
       <SetContainer>
-        <InputGroup className="input-fixedexpenses">
-          <InputLeftElement
-            pointerEvents="none"
-            children={<Icon as={GiExpense} color="gray.400" />}
-          />
-          <Input type="date" size="sm" focusBorderColor="purple.400"></Input>
-        </InputGroup>
-        <InputGroup className="input-fixedexpenses" size="sm">
+        <div>
+          <p>급여일</p>
+          <InputGroup className="input-fixedexpenses" ml="10vw">
+            <InputLeftElement
+              pointerEvents="none"
+              children={<Icon as={GiExpense} color="gray.400" />}
+            />
+            <Input type="date" size="sm" focusBorderColor="purple.400"></Input>
+          </InputGroup>
+        </div>
+        <InputGroup className="input-fixedexpenses" size="sm" ml="20vw">
           <InputLeftElement
             pointerEvents="none"
             children={<Icon as={GiExpense} color="gray.400" />}
