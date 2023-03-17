90add57480187ccf0aa27cf438704eb469dc0b3a
SSH <school023@gmail.com>
2023-03-18 02:47:33 +0900

    [add] AccountServiceImpl.java

--- tikkle-be/src/main/java/DabuOps/tikkle/account/repository/AccountRepository.java
+++ tikkle-be/src/main/java/DabuOps/tikkle/account/repository/AccountRepository.java
@@ -2,12 +2,14 @@ package DabuOps.tikkle.account.repository;
 
 import DabuOps.tikkle.account.entity.Account;
 import DabuOps.tikkle.account.entity.Account.AccountState;
-import DabuOps.tikkle.member.entity.Member;
-import DabuOps.tikkle.member.entity.Member.MemberState;
+import java.util.List;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AccountRepository {
-    Optional<Account> findAllByMemberIdAndStateIs(Long id, AccountState state);
+public interface AccountRepository extends JpaRepository<Account, Long> {
+    List<Account> findAllByMemberIdAndStateIs(Long id, AccountState state);
+
+    Optional<Account> findByIdAndStateIs(Long id, AccountState state);
 }
--- tikkle-be/src/main/java/DabuOps/tikkle/account/service/AccountServiceImpl.java
+++ tikkle-be/src/main/java/DabuOps/tikkle/account/service/AccountServiceImpl.java
@@ -1,11 +1,20 @@
 package DabuOps.tikkle.account.service;
 
 import DabuOps.tikkle.account.entity.Account;
+import DabuOps.tikkle.account.entity.Account.AccountState;
+import DabuOps.tikkle.account.repository.AccountRepository;
+import DabuOps.tikkle.global.exception.BusinessLogicException;
+import DabuOps.tikkle.global.exception.ExceptionCode;
 import java.util.List;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class AccountServiceImpl implements AccountService{
+
+    private final AccountRepository accountRepository;
 
     @Override
     public Account createAccount(Account account, Long memberId) {
@@ -14,21 +23,40 @@ public class AccountServiceImpl implements AccountService{
 
     @Override
     public Account updateAccount(Account account, Long id, Long memberId) {
-        return null;
+
+        Account obtainAccount = findExistAccountById(id);
+        Optional.ofNullable(account.getName())
+            .ifPresent(content -> obtainAccount.setName(content));
+        return accountRepository.save(obtainAccount);
     }
 
     @Override
     public List<Account> getAccounts(Long memberId) {
-        return null;
+
+        //사용자의 아이디로 계좌를 검색해서 전체 리스트로 반환
+        List<Account> accounts = accountRepository.findAllByMemberIdAndStateIs(memberId, AccountState.ACTIVE);
+
+        return accounts;
     }
 
     @Override
     public void deleteAccount(Long id, Long memberId) {
-
+        //계좌 식별자를 검색해서
+        Account obtainAccount = findExistAccountById(id);
+        // 삭제 상태로 갱신한후 저장
+        obtainAccount.setState(AccountState.INACTIVE);
+        accountRepository.save(obtainAccount);
     }
 
     @Override
     public Account findExistAccountById(Long id) {
-        return null;
+        // 계좌 식별자로 찾고
+        Optional<Account> optionalAccount = accountRepository.findByIdAndStateIs(id, AccountState.ACTIVE);
+        //없으면 에러 있으면 반환
+        Account obtainAccount = optionalAccount
+            .orElseThrow(() -> new BusinessLogicException(ExceptionCode.ACCOUNT_NOT_FOUND));
+        return obtainAccount;
     }
+
+
 }
