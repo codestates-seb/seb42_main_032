e3d8e3e2e14e87f9608329917346d5f7bcf33c13
Kyusung98 <sgs3898@gmail.com>
2023-03-18 23:47:10 +0900

    [Add] AccountInfo Structure

--- tikkle-be/src/main/java/DabuOps/tikkle/webclient/service/WebClientService.java
+++ tikkle-be/src/main/java/DabuOps/tikkle/webclient/service/WebClientService.java
@@ -2,8 +2,10 @@ package DabuOps.tikkle.webclient.service;
 
 import DabuOps.tikkle.member.entity.Member;
 import DabuOps.tikkle.member.repository.MemberRepository;
+import DabuOps.tikkle.webclient.dto.AccountInfoDto;
 import DabuOps.tikkle.webclient.dto.TokenResponseDto;
 import java.util.Optional;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -17,11 +19,11 @@ import reactor.core.publisher.Mono;
 /*
  * client 사용자 인증 요청 클릭시 핸드폰 인증을 하며 authorization_code 발급 받아 headers 에 담아서 server 로 넘겨주기 (프론트)
  * server 로 넘겨준 authorization_code 를 requestToken() 메서드로 넘긴다.(controller)
- * requestToken()에서 authorization_code 를 포함한 clientId, secret, redirectUri, grant_type 을 formData 에 담아준다
- * webClient을 이용하여 formData 를 uri로 보내준 후 TokenResponseDto 에 받아온다.
  */
 @Service
+@RequiredArgsConstructor
 public class WebClientService {
+
     private final MemberRepository memberRepository;
     WebClient webClient = WebClient.create("{openbanking.api.url}");
     @Value("{K_CLIENT_ID}")
@@ -33,10 +35,12 @@ public class WebClientService {
     @Value("{K_REDIRECT_URI}")
     private String redirectUri;
 
-    public WebClientService(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
-    }
-
+    /*
+     * requestToken()에서 authorization_code 를 포함한 clientId, secret, redirectUri, grant_type 을 formData 에 담아준다
+     * webClient을 이용하여 formData 를 uri로 보내준 후 TokenResponseDto 에 받아온다.
+     * 받아온 데이터를 memberRepository 에서 findById로 memberId 확인
+     * memberId가 확인되면 TokenResponseDto 에서 accessToken 을 가져와서 memberRepository 에 저장
+     */
     public Mono<TokenResponseDto> requestToken(String authorizationCode, Long memberId) {
         MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
         formData.add("code", authorizationCode);
@@ -63,4 +67,17 @@ public class WebClientService {
                 }
             });
     }
+
+    public Mono<AccountInfoDto> requestUserInfo(String accessToken) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setBearerAuth(accessToken);
+
+        return webClient.get()
+            .uri("/v2.0/user/me")
+            .headers(h -> h.addAll(headers))
+            .retrieve()
+            .bodyToMono(AccountInfoDto.class);
+    }
+
 }
