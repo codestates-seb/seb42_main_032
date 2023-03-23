54390e77b68650ab49e0201db783e16f99725a6c
Kyusung98 <sgs3898@gmail.com>
2023-03-23 21:20:21 +0900

    [Add] Security Config filterChain authorizedRequset /init

--- tikkle-be/src/main/java/DabuOps/tikkle/oauth/config/SecurityConfiguration.java
+++ tikkle-be/src/main/java/DabuOps/tikkle/oauth/config/SecurityConfiguration.java
@@ -26,7 +26,7 @@ public class SecurityConfiguration {
             .httpBasic().disable()
 
             .authorizeRequests()
-            .antMatchers("/","/oauth2/**","/login/**","/h2-console/**","/budgets/**","/members/**","/transaction_histories/**","/categories/**","/curations/**","/tags/**","/accounts/**")
+            .antMatchers("/","/oauth2/**","/login/**","/h2-console/**","/budgets/**","/members/**","/transaction_histories/**","/categories/**","/curations/**","/tags/**","/accounts/**", "/init/**")
             .permitAll()
             .anyRequest().authenticated()
 
