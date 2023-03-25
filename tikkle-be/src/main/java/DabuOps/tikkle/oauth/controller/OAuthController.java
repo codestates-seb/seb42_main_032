eb915005e56146f3d887c4595b915fb64534cf21
SSH <school023@gmail.com>
2023-03-25 15:25:11 +0900

    [refactoring] add @CrossOrigin annotation

--- tikkle-be/src/main/java/DabuOps/tikkle/oauth/controller/OAuthController.java
+++ tikkle-be/src/main/java/DabuOps/tikkle/oauth/controller/OAuthController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin
 @RequiredArgsConstructor
 @Slf4j
 public class OAuthController {
