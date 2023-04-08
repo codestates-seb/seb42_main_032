eaa913f2e7fe0a7308ab680fbe571de1e44fb047
Jaehun Kim <dev4jaehun.kim@gmail.com>
2023-04-08 09:16:07 +0900

    Add comment for useHrefModal

--- tikkle-fe/src/hooks/useHrefModal.tsx
+++ tikkle-fe/src/hooks/useHrefModal.tsx
@@ -8,13 +8,11 @@ export function useHrefModal(
 ) {
   const [showModal, setShowModal] = useState(false);
 
-  const onOpen = () => {
-    console.log(true);
-    setShowModal(true);
-  };
-
+  // 모달을 열고 닫는 핸들러
+  const onOpen = () => setShowModal(true);
   const onClose = () => setShowModal(false);
 
+  // 모달을 열기 위해 사용하는 코드 내에 넣어야 하는 컴포넌트
   const ModalWrapper = () => (
     <>
       {showModal &&
