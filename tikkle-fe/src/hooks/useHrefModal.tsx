fdf451da25f122b6598a0ed90447087a2e4d4f6a
Jaehun Kim <dev4jaehun.kim@gmail.com>
2023-04-08 16:05:10 +0900

    Add open modal condition

--- tikkle-fe/src/hooks/useHrefModal.tsx
+++ tikkle-fe/src/hooks/useHrefModal.tsx
@@ -1,15 +1,23 @@
 import { useState } from 'react';
 import { createPortal } from 'react-dom';
+import { useRecoilValue } from 'recoil';
 import { HrefModal } from '../components/layout/HrefModal';
+import { userInfoState } from '../util/store';
 export function useHrefModal(
   infoMessage: string,
   buttonLabel: string,
   targetPage: string
 ) {
+  const userInfo = useRecoilValue(userInfoState);
   const [showModal, setShowModal] = useState(false);
 
   // 모달을 열고 닫는 핸들러
-  const onOpen = () => setShowModal(true);
+  const onOpen = () => {
+    // 사용자가 active 상태일 때는 모달을 열지 않도록 설정
+    if (userInfo?.state !== 'active') {
+      setShowModal(true);
+    }
+  };
   const onClose = () => setShowModal(false);
 
   // 모달을 열기 위해 사용하는 코드 내에 넣어야 하는 컴포넌트
