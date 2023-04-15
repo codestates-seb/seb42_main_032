c6976033633403cc38c7c1e090785e5c0b8017ac
Jaehun Kim <dev4jaehun.kim@gmail.com>
2023-04-15 17:06:09 +0900

    Add SearchBar style

--- tikkle-fe/src/components/curation/SearchBar.tsx
+++ tikkle-fe/src/components/curation/SearchBar.tsx
@@ -1,11 +1,32 @@
-import styled from "styled-components"
+import styled from 'styled-components';
+
+import { MdClear, MdKeyboardArrowDown } from 'react-icons/md';
+import { useState } from 'react';
 
 export default function SearchBar() {
+  const [keyword, setKeyword] = useState('');
+
   return (
     <SearchBarBox>
-
+      <SearchType>
+        <span>제목+태그</span>
+        <div className="dropdown-button">
+          <MdKeyboardArrowDown />
+        </div>
+      </SearchType>
+      <input
+        type="text"
+        value={keyword}
+        onChange={(e: React.ChangeEvent) => {
+          setKeyword((e.target as HTMLInputElement).value);
+        }}
+      />
+      <ClearButton>
+        <MdClear />
+      </ClearButton>
+      <SearchButton>검색</SearchButton>
     </SearchBarBox>
-  )
+  );
 }
 
 const SearchBarBox = styled.div`
@@ -13,30 +34,36 @@ const SearchBarBox = styled.div`
   height: 3.75rem;
   border: 0.125rem solid black;
   border-radius: 1.25rem;
-  
+
   font-family: GMarketSansMedium;
 
   display: flex;
   align-items: center;
-  
+
   input {
     flex-grow: 1;
+    height: 100%;
     display: flex;
     align-items: center;
+  }
+
+  svg {
+    font-size: 1.5rem;
   }
 `;
 
 const SearchType = styled.div`
-  width: 5.625rem;
   height: 100%;
   display: flex;
   align-items: center;
+  gap: 0.5rem;
+  margin-left: 0.5rem;
 
   span {
     text-align: center;
     text-decoration: underline;
   }
-  
+
   .dropdown-button {
     display: flex;
     justify-content: center;
@@ -50,15 +77,16 @@ const ClearButton = styled.div`
   border-radius: 100%;
   background-color: #805ad5
   color: white;
-  display: flex
+  display: flex;
+  justify-content: center;
   align-items: center;
 `;
 
 const SearchButton = styled.div`
   width: 5.625rem;
   height: 100%;
-  border-radius: 0 1.25rem 1.25rem 0;
-  background-color: #805ad5
+  border-radius: 0 1.1rem 1.1rem 0;
+  background-color: #805ad5;
   color: white;
   display: flex;
   justify-content: center;
