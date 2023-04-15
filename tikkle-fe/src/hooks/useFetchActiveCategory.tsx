0defb464f3f338db1880051697360b6e444321be
Jaehun Kim <dev4jaehun.kim@gmail.com>
2023-04-15 22:49:01 +0900

    Remove passed id by parameter

--- tikkle-fe/src/hooks/useFetchActiveCategory.tsx
+++ tikkle-fe/src/hooks/useFetchActiveCategory.tsx
@@ -1,13 +1,20 @@
 import axios from 'axios';
+import { useRecoilValue } from 'recoil';
+import { userInfoState } from '../util/store';
 
-const useFetchActiveCategory = async (id: number) => {
+// 활성화 된 카테고리만 조회하는 훅
+// 예산 정보는 활성화된 카테고리만 조회되므로, 예산을 불러오고, 불러온 예산정보와 일치하는 카테고리만 뽑음
+const useFetchActiveCategory = async () => {
+  const userInfo = useRecoilValue(userInfoState);
   try {
     const memberBudget = await (
-      await axios.get(`${import.meta.env.VITE_SERVER}/budgets/members/${id}`)
+      await axios.get(
+        `${import.meta.env.VITE_SERVER}/budgets/members/${userInfo?.id}`
+      )
     ).data;
 
     const allCategories = await axios.get(
-      `${import.meta.env.VITE_SERVER}/categories/${id}`
+      `${import.meta.env.VITE_SERVER}/categories/${userInfo?.id}`
     );
     const activeCategory = [];
     for (const i of memberBudget) {
