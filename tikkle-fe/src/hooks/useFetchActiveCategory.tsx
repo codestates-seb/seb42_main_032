6344c52c24450a340fb40b4ba206b820ed0a47bd
Jaehun Kim <dev4jaehun.kim@gmail.com>
2023-04-16 00:44:12 +0900

    Refactor useFetchActiveCategory

--- tikkle-fe/src/hooks/useFetchActiveCategory.tsx
+++ tikkle-fe/src/hooks/useFetchActiveCategory.tsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { useRecoilValue } from 'recoil';
+import { BudgetType, CategoryType } from '../pages/BudgetSetting';
 import { userInfoState } from '../util/store';
 
 // 활성화 된 카테고리만 조회하는 훅
@@ -7,27 +8,33 @@ import { userInfoState } from '../util/store';
 const useFetchActiveCategory = async () => {
   const userInfo = useRecoilValue(userInfoState);
   try {
-    const memberBudget = await (
+    const memberBudget = (
       await axios.get(
         `${import.meta.env.VITE_SERVER}/budgets/members/${userInfo?.id}`
       )
     ).data;
 
-    const allCategories = await axios.get(
-      `${import.meta.env.VITE_SERVER}/categories/${userInfo?.id}`
+    const allCategories = (
+      await axios.get(
+        `${import.meta.env.VITE_SERVER}/categories/${userInfo?.id}`
+      )
+    ).data.data;
+
+    // 활성화된 카테고리만 추출
+    const activeBudget = memberBudget.filter(
+      (budget: BudgetType) => budget.status === 'ACTIVE'
     );
-    const activeCategory = [];
-    for (const i of memberBudget) {
-      for (const j of allCategories?.data.data) {
-        if (i.memberCategoryId === j.id && i.status === 'ACTIVE') {
-          activeCategory.push({ ...j, ...i });
-          break;
-        }
-      }
-    }
+
+    // 카테고리의 ID가 활성화된 예산의 멤버 카테고리 id로 존재하는 경우만 추출
+    const activeCategory = allCategories.filter(
+      (category: CategoryType) =>
+        category.id ===
+        activeBudget.includes((budget: BudgetType) => budget.memberCategoryId)
+    );
+
     return activeCategory;
   } catch (err) {
-    return err;
+    console.log(err);
   }
 };
 
